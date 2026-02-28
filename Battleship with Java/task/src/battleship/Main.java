0c55f65a833dd0f185e024735cbb8cd07ce7a193
Anne Flore Bernard <b.anneflore@yahoo.fr>
2026-02-28 12:42:01 +0100

    new start

--- Battleship with Java/task/src/battleship/Main.java
+++ Battleship with Java/task/src/battleship/Main.java
@@ -3,11 +3,33 @@ package battleship;
 
 import java.util.Scanner;
 
-import static battleship.SeaManagement.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        displayBoardAndQuestion();
+        Scanner scanner = new Scanner(System.in);
+        Ship[] playerOneShips = {
+                new Ship(5, "Aircraft Carrier"),
+                new Ship(4, "Battleship"),
+                new Ship(3, "Submarine"),
+                new Ship(3, "Cruiser"),
+                new Ship(2, "Destroyer")
+        };
+
+        for(int i = 0; i < playerOneShips.length; i++) {
+            placeShipPrompt(playerOneShips[i]);
+            String startCoordinate = scanner.next();
+            String endCoordinate = scanner.next();
+        }
+
     }
+    
+    public static void placeShipPrompt(Ship ship) {
+        System.out.format("Enter the coordinates of the %s (%d cells):\n", ship.name, ship.cells);
+    }
+
+    public void isValidCoordinate(String startCoordinate, String endCoordinate) {
+    }
+    
+
 }
--- Battleship with Java/task/src/battleship/Ship.java
+++ Battleship with Java/task/src/battleship/Ship.java
@@ -2,29 +2,21 @@ package battleship;
 
 public class Ship {
     int cells;
-    String nameShip;
+    String name;
 
-    enum BoatsType {
-        AIRCRAFT("Aircraft Carrier",5), BATTLESHIP("Battleship",4), SUBMARINE("Submarine",3), CRUISER("Cruiser", 3), DESTROYER("Destroyer",2);
-        private final int cells;
-        private final String nameShip;
-
-        BoatsType(String nameShip, int cells) {
-            this.nameShip = nameShip;
-            this.cells = cells;
-        }
+    public Ship (int cells, String name ) {
+        this.cells = cells;
+        this.name = name;
     }
 
-    public Ship(BoatsType shipType) {
-        this.cells = shipType.cells;
-        this.nameShip = shipType.nameShip;
-    }
+
+
 
     public int getCells() {
         return cells;
     }
 
     public String getNameShip() {
-        return nameShip;
+        return name;
     }
 }
