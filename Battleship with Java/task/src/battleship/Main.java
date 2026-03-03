a4e052a388c7142727fe3edd8c92b32ca76801aa
Anne Flore Bernard <b.anneflore@yahoo.fr>
2026-03-03 08:34:11 +0100

    parseStringCoordinate

--- Battleship with Java/task/src/battleship/Main.java
+++ Battleship with Java/task/src/battleship/Main.java
@@ -2,6 +2,7 @@ package battleship;
 
 
 import java.util.Scanner;
+import java.util.HashMap;
 
 
 public class Main {
@@ -26,6 +27,27 @@ public class Main {
     
     public static void placeShipPrompt(Ship ship) {
         System.out.format("Enter the coordinates of the %s (%d cells):\n", ship.name, ship.cells);
+    }
+
+    public int[] parseStringCoordinate(String stringCoordinate) {
+        HashMap<String, Integer> letterMapping = new HashMap<>();
+        letterMapping.put("A", 0);
+        letterMapping.put("B", 1);
+        letterMapping.put("C", 2);
+        letterMapping.put("D", 3);
+        letterMapping.put("E", 4);
+        letterMapping.put("F", 5);
+        letterMapping.put("G", 6);
+        letterMapping.put("H", 7);
+        letterMapping.put("I", 8);
+        letterMapping.put("J", 9);
+
+
+        String letter = String.valueOf(stringCoordinate.charAt(0));
+        int number = Integer.parseInt(stringCoordinate.substring(1));
+        int letterDico = letterMapping.get(letter);
+        int[] coordinate = {letterDico, number};
+        return coordinate;
     }
 
     public void isValidCoordinate(String startCoordinate, String endCoordinate) {
